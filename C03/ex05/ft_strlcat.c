be506d285892b2f414676c0adffc376b6a6f64ff
Ana Belosevich <anabelosevich@macbook-air-ana.home>
2025-02-25 12:39:17 +0100

    strlcat

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -14,32 +14,38 @@ unsigned int ft_strlen(char *s)
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int dst_len;
-	int src_len;
+	unsigned int dst_len;
+	unsigned int src_len;
 	unsigned int j;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dest);
 
-	if (size == 0)
-		return (src_len);
-
+	if (size <= dst_len)
+		return (src_len + size);
+	else if (size == dst_len + 1)
+		return (src_len + dst_len);
+	
 	j = 0;
-	while (j < size - 1 && dest[dst_len + j - 1] && src[j])
+	while (j < size - dst_len - 1 && src[j])
 	{
-		dest[dst_len + j - 1] = src[j];
+		dest[dst_len + j] = src[j];
 		j++;
 	}
-	dest[dst_len + j - 1] = '\0';
-	return (ft_strlen(dest));
+	dest[dst_len + j] = '\0';
+	return (src_len + dst_len);
 }
 
 int main()
 {
-	char src[] = "45645";
-	char dst[10] = "1234";
-	printf("res %s\n", dst);
-	printf("strlcat res %lu\n", strlcat(dst, src, 3));
-	printf("ft_strlcat res %u\n", ft_strlcat(dst, src, 3));
+	char src[] = "4567"; //4
+	char my_dst[10] = "123"; //3
+	char dst[10] = "123";
+	unsigned int size = 6;
+
+	printf("strlcat res %lu\n", strlcat(dst, src, size));
+	printf("ft_strlcat res %u\n", ft_strlcat(my_dst, src, size));
+	printf("res strlcat %s\n", dst);
+	printf("res ft_strlcat %s\n", my_dst);
 	return 0;
 }
