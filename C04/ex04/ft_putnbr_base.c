5f89b38ef2eb1238da26bbc8ea8b03e11dc4994c
Ana Belosevich <anabelosevich@macbook-air-ana.home>
2025-02-27 16:18:31 +0100

    rendre

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -28,13 +28,45 @@ int base_check(char *base)
 	return (0);
 }
 
+int ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);
+}
+
 
 void ft_putnbr_base(int nbr, char *base)
 {
-	
+	char c;
+	int base_len;
+	long tmp;
+
+	tmp = (long)nbr;
+	base_len = ft_strlen(base);
+	if (tmp < 0)
+	{
+		tmp *= -1;
+		write(1, "-", 1);
+	}
+	if (tmp >= base_len)
+	{
+		ft_putnbr_base(tmp / base_len, base);
+		tmp = tmp % base_len;
+	}
+	c = base[tmp];
+	write(1, &c, 1);
 }
 
 int main()
 {
-	
+	//ft_putnbr_base(127, "0123456789ABCDEF"); //7F
+	//ft_putnbr_base(127, "01"); //1111111
+	//ft_putnbr_base(2147483647, "0123456789");
+	//ft_putnbr_base(127, "poneyvif"); //off
+	ft_putnbr_base(-2147483648, "0123456789");
+	return 0;
 }
