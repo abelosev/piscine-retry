cfa1d6f9fc21debc361e8d4b0bca2140eda79f56
Anastasiia Belosevich <anbelose@made-f0Ar4s3.clusters.42paris.fr>
2025-03-03 23:36:34 +0100

    02

--- C07/ft_ultimate_range.c
+++ C07/ft_ultimate_range.c
@@ -21,15 +21,33 @@ void print_tab(int *tab, int size)
 int ft_ultimate_range(int **range, int min, int max)
 {
 
+    int size;
+    int i;
 
+    if (min >= max)
+    {
+        range = NULL;
+        return (0);
+    }
+    size = max - min - 1;
+    *range = malloc(sizeof(int) * size);
+    if (!*range)
+        return (-1);
+    i = 0;
+    while(i < size) // why min < max doesn't work? (in case of min++)
+    {
+        (*range)[i] = min + 1 + i;
+        i++;
+    }
+    return (size);
 }
 
 int main()
 {
-    int min = 2;
-    int max = 5;
-    int *res = NULL;
-    printf("%d\n", ft_ultimate_range(&res, min, max));
-    print_tab(res, max - min - 1);
+    int min = 5;
+    int max = -5;
+    int *range = NULL;
+    ft_ultimate_range(&range, min, max);
+    print_tab(range, max - min - 1);
     return (0);
 }
