de4a4f4d3e14e2a2374b9b939d5c385c0f3d042b
Anastasiia Belosevich <anbelose@made-f0Br6s9.clusters.42paris.fr>
2025-03-04 14:32:54 +0100

    123

---
+++ C04/ft_fibonacci.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anbelose <anbelose@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 14:31:58 by anbelose          #+#    #+#             */
+/*   Updated: 2025/03/04 14:32:00 by anbelose         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+// int main()
+// {
+//	 int i = 4;
+//	 printf("%d\n", ft_fibonacci(i));
+//	 return 0;
+// }
